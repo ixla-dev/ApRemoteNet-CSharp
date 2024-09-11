375df17170737c36e7716357310fd3f78f3ed5f6
pablo.cirillo <pablo.cirillo@ixlaengineering.it>
2024-09-11 13:40:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Wrapper;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var aprint = new apRemoteNet.APRemote_Net();
+        aprint.ConnectServer("127.0.0.1", 1025);
+        aprint.OpenPrintDialog(true);
+    }
+}
